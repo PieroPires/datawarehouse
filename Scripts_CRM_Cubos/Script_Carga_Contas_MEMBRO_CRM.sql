dfefbf1d443cfd835c871090852708e44aa438e4
Fiama dos Santos Cristi <fiama.cristi@vagas.com.br>
2017-09-29 19:06:34 -0300

    Substituição do PROPRIETÁRIO da OPORTUNIDADE pelo PROPRIETÁRIO da CONTA no campo GR da visão de CLIENTES

--- Scripts_CRM_Cubos/Script_Carga_Contas_MEMBRO_CRM.sql
+++ Scripts_CRM_Cubos/Script_Carga_Contas_MEMBRO_CRM.sql
@@ -14,7 +14,11 @@ SELECT	CONVERT(A.id USING Latin1) AS CONTA_ID ,
 		CONVERT(B.tipo_c USING Latin1) AS TIPO ,
         CONVERT(A.account_type USING Latin1) AS CATEGORIA ,
         A.date_entered AS DATA_ENTRADA ,
-        B.porcentual_rateio_c AS PORCENTUAL_RATEIO
+        B.porcentual_rateio_c AS PORCENTUAL_RATEIO ,
+        CONVERT(B.mercado_c USING Latin1) AS MERCADO ,
+        (SELECT	CONVERT(A1.user_name USING Latin1) AS PROPRIETARIO_CONTA
+		 FROM	sugarcrm.users AS A1
+         WHERE	A.assigned_user_id = A1.id) AS PROPRIETARIO_CONTA
 FROM	sugarcrm.accounts AS A		LEFT OUTER JOIN sugarcrm.accounts_cstm AS B ON A.id = B.id_c
 WHERE	IFNULL(A.parent_id, '') != '' # Conta Membro
 		AND A.deleted = 0 ;
