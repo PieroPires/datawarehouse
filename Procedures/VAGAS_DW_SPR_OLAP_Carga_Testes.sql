df42a616941b6a8fa21046c7513e54339e972172
Fiama dos Santos Cristi <fiama.cristi@vagas.com.br>
2018-10-17 11:43:20 -0300

    Procedure para carga de registros na visão TESTES

---
+++ Procedures/VAGAS_DW_SPR_OLAP_Carga_Testes.sql
@@ -0,0 +1,23 @@
+-- =============================================
+-- Author: Fiama
+-- Create date: 18/06/2018
+-- Description: Procedure para carga das tabelas temporárias (BD Stage) para alimentação do DW
+-- =============================================
+
+USE [VAGAS_DW] ;
+GO
+
+IF EXISTS (SELECT * FROM SYS.OBJECTS WHERE NAME = 'SPR_OLAP_Carga_Testes' AND SCHEMA_NAME(SCHEMA_ID) = 'VAGAS_DW')
+DROP PROCEDURE [VAGAS_DW].[SPR_OLAP_Carga_Testes]
+GO
+
+CREATE PROCEDURE [VAGAS_DW].[SPR_OLAP_Carga_Testes]
+AS
+SET NOCOUNT ON
+
+	-- Limpar tabela FATO:
+	TRUNCATE TABLE [VAGAS_DW].[TESTES] ;
+
+	-- Inserindo registros na visão:
+	INSERT INTO [VAGAS_DW].[TESTES] (COD_TESTE, NOME_TESTE, COD_CLI_TESTE, GLOBAL_TESTE, IDIOMA_TESTE)
+	SELECT	* FROM [STAGE].[VAGAS_DW].[TMP_TESTES] ;
