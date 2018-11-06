2f55f945f0dec84f2ee7477daaa00f3cee885218
Fiama dos Santos Cristi <fiama.cristi@vagas.com.br>
2018-11-06 14:18:35 -0200

    Visão de EVENTOS PRESENCIAIS (serviços sobre marcados)

--- Procedures/Stage_SPR_OLTP_Carga_Eventos_Presenciais.sql
+++ Procedures/Stage_SPR_OLTP_Carga_Eventos_Presenciais.sql
@@ -19,7 +19,7 @@ SET NOCOUNT ON
 TRUNCATE TABLE [VAGAS_DW].[TMP_EVENTOS_PRESENCIAIS] ;
 
 -- Carrega os dados na tabela FATO:
-INSERT INTO [VAGAS_DW].[TMP_EVENTOS_PRESENCIAIS]
+INSERT INTO [VAGAS_DW].[TMP_EVENTOS_PRESENCIAIS] (COD_EVENTO,NOME_EVENTO,COD_VAGA,TIPO,DATA_ENVIO_AGENDAMENTO,QTD_EVENTOS,QTD_ENVIO_AGENDAMENTO)
 SELECT	A.Cod_agEv AS COD_EVENTO ,
 		A.Nome_agEv AS NOME_EVENTO ,
 		A.CodVaga_agEv AS COD_VAGA ,
@@ -28,5 +28,19 @@ SELECT	A.Cod_agEv AS COD_EVENTO ,
 				THEN 'PRESENCIAL'
 			WHEN A.FormaParticipacao_agEv = 1
 				THEN 'REMOTO'
-		END AS TIPO
-FROM	[hrh-data].[dbo].[Agendamento-Eventos] AS A ;
+		END AS TIPO ,
+		CONVERT(DATE, B.DataPed_agPend) AS DATA_ENVIO_AGENDAMENTO ,
+		COUNT(DISTINCT A.Cod_agEv) AS QTD_EVENTOS ,
+		COUNT(*) AS QTD_ENVIO_AGENDAMENTO
+FROM	[hrh-data].[dbo].[Agendamento-Eventos] AS A		INNER JOIN [hrh-data].[dbo].[Cand-AgendamentosPend] AS B ON A.Cod_agEv = B.CodEv_agPend
+GROUP BY
+		A.Cod_agEv ,
+		A.Nome_agEv ,
+		A.CodVaga_agEv ,
+		CASE
+			WHEN A.FormaParticipacao_agEv = 0
+				THEN 'PRESENCIAL'
+			WHEN A.FormaParticipacao_agEv = 1
+				THEN 'REMOTO'
+		END ,
+		CONVERT(DATE, B.DataPed_agPend) ;
