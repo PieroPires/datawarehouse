8cc18c7deacaacb97e191cf550c774613cf59a07
Fiama dos Santos Cristi <fiama.cristi@vagas.com.br>
2019-01-08 11:48:54 -0200

    Carga do Status Global do CV

--- Procedures/Stage_SPR_OLTP_Carga_Status_Global.sql
+++ Procedures/Stage_SPR_OLTP_Carga_Status_Global.sql
@@ -18,16 +18,11 @@ SET NOCOUNT ON
 TRUNCATE TABLE [VAGAS_DW].[TMP_STATUS_GLOBAL_VEP] ;
 
 INSERT INTO [VAGAS_DW].[TMP_STATUS_GLOBAL_VEP]
-SELECT	A.CodCliente_candCli AS COD_CLI ,
-		CONVERT(SMALLDATETIME, CONVERT(CHAR(10), A.DtIniStatus_candCli, 112)) AS DATA_STATUS_GLOBAL ,
-		A.VagaStatus_candCli AS COD_VAGA ,
+SELECT	A.CodCliente_hist AS COD_CLI ,
+		CONVERT(SMALLDATETIME, CONVERT(CHAR(10), A.Dt_hist, 112)) AS DATA_STATUS_GLOBAL ,
 		COUNT(*) AS QTD_ALTERACOES_STATUS_GLOBAL
-FROM	[hrh-data].[dbo].[CandidatoxCliente] AS A
-WHERE	EXISTS ( SELECT	1
-				 FROM	[hrh-data].[dbo].[Cad_stGlobalCand] AS A1
-				 WHERE	A.CodStatus_candCli = A1.Cod_stGCand
-						AND A.CodCliente_candCli = A1.CodCli_stGCand )
+FROM	[hrh-data].[dbo].[Historico] AS A
+WHERE	A.Tipo_hist = -114
 GROUP BY
-		A.CodCliente_candCli ,
-		CONVERT(SMALLDATETIME, CONVERT(CHAR(10), A.DtIniStatus_candCli, 112)) ,
-		A.VagaStatus_candCli ;
+		A.CodCliente_hist ,
+		CONVERT(SMALLDATETIME, CONVERT(CHAR(10), A.Dt_hist, 112)) ;
