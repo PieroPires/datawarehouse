106ef6719af455cca3b95be8bfba632b45b5d9fd
Diego Henrique de Souza Gatto <diego.gatto@vagas.com.br>
2019-06-11 09:08:58 -0300

    Adicionado QTD_CURRICULOS na Triagem

--- Procedures/Stage_SPR_OLTP_Carga_Triagens_Vagas.sql
+++ Procedures/Stage_SPR_OLTP_Carga_Triagens_Vagas.sql
@@ -43,7 +43,8 @@ SELECT	CONVERT(DATE, A.Data_debTri) AS DATA_TRIAGEM ,
 			WHEN A.VersaoTriagem_debTri = 2
 				THEN 'TRIAGEM 2.0'
 		END AS VERSAO_TRIAGEM ,
-		COUNT(*) AS QTD_TRIAGENS
+		COUNT(*) AS QTD_TRIAGENS ,
+		SUM(QTDECV_DEBTRI) AS QTD_CURRICULOS
 INTO	#TMP_TRIAGENS_VAGAS
 FROM	[hrh-data].[dbo].[DebugTriagem] AS A	LEFT OUTER JOIN [hrh-data].[dbo].[Vagas] AS B ON A.CodVaga_debTri = B.Cod_vaga
 												AND B.VagaModelo_vaga = 0 -- Vaga não é modelo   
@@ -55,6 +56,7 @@ WHERE	A.Data_debTri >= '19000101'
 		AND A.SenhaMestre_debTri = 0 -- Não foi realizado por acesso Manut
 		AND ( A.VersaoTriagem_debTri IS NULL -- Triagem 1.0
 			  OR A.VersaoTriagem_debTri = 2 ) -- Triagem 2.0
+		AND CODVAGA_DEBTRI > 0
 GROUP BY
 		CONVERT(DATE, A.Data_debTri) ,
 		A.CodVaga_debTri ,
@@ -79,12 +81,13 @@ GROUP BY
 		END ;
 
 -- Carregar registros na tabela temporária:
-INSERT INTO [VAGAS_DW].[TMP_TRIAGENS_VAGAS](DATA_TRIAGEM,COD_VAGA,CONTEXTO,COD_CLI,CLIENTE_VAGAS,VERSAO_TRIAGEM,QTD_TRIAGENS)
+INSERT INTO [VAGAS_DW].[TMP_TRIAGENS_VAGAS](DATA_TRIAGEM,COD_VAGA,CONTEXTO,COD_CLI,CLIENTE_VAGAS,VERSAO_TRIAGEM,QTD_TRIAGENS, QTD_CURRICULOS)
 SELECT	A.DATA_TRIAGEM ,
 		A.COD_VAGA ,
 		A.CONTEXTO ,
 		A.COD_CLI ,
 		A.CLIENTE_VAGAS ,
 		A.VERSAO_TRIAGEM ,
-		A.QTD_TRIAGENS
+		A.QTD_TRIAGENS ,
+		A.QTD_CURRICULOS
 FROM #TMP_TRIAGENS_VAGAS AS A ;
